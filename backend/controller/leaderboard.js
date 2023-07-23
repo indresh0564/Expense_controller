f58f1ec2c7c5c02bcbad3ee99d26ee82f5623892
indresh0564 <indresh0564@gmail.com>
2023-07-23 23:36:36 +0530

    update totalExpenses in the user table

--- backend/controller/leaderboard.js
+++ backend/controller/leaderboard.js
@@ -6,13 +6,13 @@ const { expense } = require('./expense');
 exports.leaderboard = async(req,res,next)=>{
     
     const users_leaderboard =await User.findAll({
-        attributes:['id' , 'name' , [sequelize.fn('sum' , sequelize.col('expense')) , 'totalExpense']],
-        include:[{
-            model:Expense,
-            attributes:[]  
-        }],
-        group:['user.id'],
-        order:['totalExpense', 'DESC']
+        attributes:['id' , 'name' , 'totalExpense']
+        // include:[{
+        //     model:Expense,
+        //     attributes:[]  
+        // }],
+        // group:['user.id'],
+        // order:['totalExpense', 'DESC']
     });
 
     res.status(201).json(users_leaderboard);
--- backend/models/user.js
+++ backend/models/user.js
@@ -29,6 +29,9 @@ const User = sequelize.define('user',{
         type:Sequelize.STRING,
         allowNull:false,
     },
+
+    totalExpense:Sequelize.INTEGER,
+
     ispremiumuser:Sequelize.BOOLEAN
 
 });
